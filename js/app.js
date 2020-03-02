ccd888be9a56c8da3ca0a212dd03bb82bbcc2da1
JhonTorres <fre920@hotmail.com>
2020-03-02 15:26:19 -0500

    Dejar por fuera la solucion

---
+++ js/app.js
@@ -0,0 +1,115 @@
+const dvBase = document.getElementById("dvBase");
+dvBase.style.display = 'none';
+
+const formulario = document.getElementById('formulario');
+/*Submit generar figura*/ 
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const Altura = document.getElementById("pAltura").value; 
+    const Base = document.getElementById("pBase").value;
+    const Radio = document.getElementById("pRadio").value;
+    const Ancho = document.getElementById("pAncho").value;
+    const Largo = document.getElementById("pLargo").value;
+    const ResultadoArea = document.getElementById("resArea");
+    const ResultadoPerimetro = document.getElementById("resPerimetro");
+
+    let miLienzo = document.getElementById("mipanel");
+    let lapiz = miLienzo.getContext("2d");
+
+    debugger
+    e.preventDefault();
+    const x = document.getElementById("Figuras").value;
+
+    lapiz.clearRect(0, 0, miLienzo.width, miLienzo.height);
+
+        switch (x) {
+            case 'Cuadrado':            
+                ResultadoArea.innerHTML = Altura * Altura;
+                ResultadoPerimetro.innerHTML  =  (Altura * 4);                     
+                lapiz.beginPath();	 
+                lapiz.rect(30,5,Altura,Altura);
+                lapiz.textAlign = "center"; 
+                lapiz.stroke();
+      
+                break;        
+            case 'Circulo':            
+                ResultadoArea.innerHTML = (Math.PI * Math.pow(Radio, 2));
+                ResultadoPerimetro.innerHTML  =  Math.PI * (2 * Radio);               
+                lapiz.beginPath();	  
+                lapiz.arc(300,150,Radio, 0, 2*Math.PI);
+                lapiz.lineWidth = 5;
+                lapiz.stroke();
+                break;  
+            case 'Triangulo':            
+                ResultadoArea.innerHTML = Math.sqrt(Math.pow(Altura, 2) + Math.pow(Base, 2)) + (Altura + Base);
+                ResultadoPerimetro.innerHTML  =  (Altura * 4);
+                // Stroked triangle
+                let height = 200 * Math.cos(Math.PI / 3);
+                lapiz.beginPath();
+                lapiz.moveTo(100, 300);
+                lapiz.lineTo(300, 300);
+                lapiz.lineTo(200, 300 - height);
+                lapiz.closePath();
+                lapiz.stroke();    
+                break;
+            case 'Rectangulo':            
+                ResultadoArea.innerHTML = (Altura * Base) ;
+                ResultadoPerimetro.innerHTML  =  ((Altura * 2) + (Base * 2));
+                lapiz.beginPath();	 
+                lapiz.rect(30,5,Altura,Base);
+                lapiz.stroke();
+                break;    
+            default:
+                break;
+        }  
+});
+/*Change al cambiar de figura*/ 
+document.querySelector('#Figuras').addEventListener('change', function(e) {
+
+    e.preventDefault();
+    let miLienzo = document.getElementById("mipanel");
+    let lapiz = miLienzo.getContext("2d");
+    lapiz.clearRect(0, 0, miLienzo.width, miLienzo.height);
+
+    const ResultadoArea = document.getElementById("resArea");
+    const ResultadoPerimetro = document.getElementById("resPerimetro");
+    ResultadoArea.innerHTML = '';
+    ResultadoPerimetro.innerHTML = '';
+
+    var els = document.getElementsByClassName("parametro");
+
+        Array.prototype.forEach.call(els, function(el) {
+            // Do stuff here
+            debugger;
+            el.style.display ='none';
+        });
+
+    const dvAltura = document.getElementById("dvAltura"); 
+    const dvBase = document.getElementById("dvBase");
+    const dvRadio = document.getElementById("dvRadio");
+    const dvAncho = document.getElementById("dvAncho");
+    const dvLargo = document.getElementById("dvLargo");
+
+    const x =  e.srcElement.value;
+    
+    switch (x) {
+        case 'Cuadrado':            
+            dvAltura.style.display = 'block';
+            break;        
+        case 'Circulo':            
+            dvRadio.style.display = 'block';
+            break;  
+        case 'Triangulo':            
+            dvAltura.style.display = 'block';
+            dvBase.style.display = 'block';
+            break;
+        case 'Rectangulo':            
+            dvAltura.style.display = 'block';
+            dvBase.style.display = 'block';
+            break;    
+        default:
+            break;
+    }  
+
+});
---
+++ pull_request_template.md
@@ -0,0 +1,11 @@
+# Descripcion
+
+¿Que ha cambiado
+
+- [ ] Frontend
+- [ ] BackEnd
+- [ ] Configuracion del Server
+
+# Como pruedo probar los cambio
+
+En que url y forma puedo ver el update
